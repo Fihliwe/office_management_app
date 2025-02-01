a62589adfd19ef47b31cad42677b3dead9fb4477
Fihliwe <136016375+Fihliwe@users.noreply.github.com>
2025-02-01 15:58:59 +0200

    us lint fixed

--- app/office/[id]/page.tsx
+++ app/office/[id]/page.tsx
@@ -3,8 +3,6 @@ import { useParams } from 'next/navigation';
 import OfficeDetail from '../../componentForms/OfficeDetail';
 
 export default function Home2() {
-  const { id } = useParams();
-
   return (
     <div>
       <OfficeDetail />
