8654bdbf2ada8e420ed01c5f4a0ba26ea79aba1f
Fihliwe <136016375+Fihliwe@users.noreply.github.com>
2025-02-02 23:09:58 +0200

    Nextjs localstorage was being accesed during SSR, modified the code

--- app/localStorage/getStoredOffices.js
+++ app/localStorage/getStoredOffices.js
@@ -6,30 +6,36 @@ const useStoredOffices = () => {
   useEffect(() => {
     setHydrated(true);
   }, []);
-  if (typeof window != hydrated) {
-    const stored = localStorage.getItem('offices');
-    return stored ? JSON.parse(stored) : [
-      {
-        id: 1,
-        name: 'Specno',
-        color: '#0EA5E9',
-        email: 'info@specno.com',
-        phone: '082 364 9984',
-        capacity: 25,
-        address: '10 Wille van Schoor Dr, Bo-Oakdale, Cape Town, 7530',
-        staffMembers: [
-          { id: 1, firstName: 'Jacques', lastName: 'Jordaan', avatar: 1 },
-          { id: 2, firstName: 'Daniel', lastName: 'Novitzkas', avatar: 2 },
-          { id: 3, firstName: 'Brandon', lastName: 'Watkins', avatar: 3 },
-          { id: 4, firstName: 'Ryan', lastName: 'Duell', avatar: 4 },
-          { id: 5, firstName: 'Jenner', lastName: 'Venter', avatar: 5 },
-          { id: 6, firstName: 'Janke B', lastName: 'Du Toit', avatar: 6 }
-        ]
-      }
-    ];
-  }
 
-  return []; // Default to an empty array if `window` is undefined (SSR)
+  
+  useEffect(() => {
+    if (typeof window !== hydrated) {
+      const stored = localStorage.getItem('offices');
+      setStoredOffices(stored ? JSON.parse(stored) : [
+        {
+          id: 1,
+          name: 'Specno',
+          color: '#0EA5E9',
+          email: 'info@specno.com',
+          phone: '082 364 9984',
+          capacity: 25,
+          address: '10 Wille van Schoor Dr, Bo-Oakdale, Cape Town, 7530',
+          staffMembers: [
+            { id: 1, firstName: 'Jacques', lastName: 'Jordaan', avatar: 1 },
+            { id: 2, firstName: 'Daniel', lastName: 'Novitzkas', avatar: 2 },
+            { id: 3, firstName: 'Brandon', lastName: 'Watkins', avatar: 3 },
+            { id: 4, firstName: 'Ryan', lastName: 'Duell', avatar: 4 },
+            { id: 5, firstName: 'Jenner', lastName: 'Venter', avatar: 5 },
+            { id: 6, firstName: 'Janke B', lastName: 'Du Toit', avatar: 6 }
+          ]
+        }
+      ]);
+    }
+  }, []);
+
+  const [storedOffices, setStoredOffices] = useState([]);
+
+  return storedOffices;
 };
 
 export default useStoredOffices;
